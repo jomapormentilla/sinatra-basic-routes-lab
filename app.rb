24651679dcb2acfec5bb8b7708ac720c15ec450f
jomapormentilla <jomapormentilla@gmail.com>
2020-11-23 15:59:30 -0500

    commit tree 5171f4ec9ea5527f296f0249970d600362adedfa

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Jomes"
+    end
+
+    get '/hometown' do
+        "My hometown is Jersey City" 
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Hotel California"
+    end
 end
